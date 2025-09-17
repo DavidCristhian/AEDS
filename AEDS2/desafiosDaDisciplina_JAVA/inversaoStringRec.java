3b8a6719cda643503dac2666c6d9dc461258eb04
David Cris <davidcristhian576@gmail.com>
2025-09-17 10:50:16 -0300

    Criação do desafio inversaoStringRec/java AEDS2, e modificacão no arquivo somaDigitos/java

--- AEDS2/desafiosDaDisciplina_JAVA/inversaoStringRec.java
+++ AEDS2/desafiosDaDisciplina_JAVA/inversaoStringRec.java
@@ -2,29 +2,23 @@ package AEDS.AEDS2.desafiosDaDisciplina_JAVA;
 
 import java.util.Scanner;
 
-public class somaDigitos{
+public class inversaoStringRec{
 
     public static boolean flegStop(String str){ 
         return(str.length() == 3 && str.charAt(0) == 'F' &&
                str.charAt(1) == 'I' && str.charAt(2) == 'M');
     }
 
-    public static int stringParaInteiro(String str){
-        int num = 0;
-
-        for(int i = 0; i < str.length(); i++){
-            num = num * 10 + (str.charAt(i) - '0');
+    public static String inversorStringRec(String str, int tam){
+        if(tam < 0){
+            return "";
         }
 
-        return num;
+        return str.charAt(tam) + inversorStringRec(str, tam - 1);
     }
 
-    public static int somaDigitosRec(int num){
-        if(num == 0){
-            return 0;
-        }
-
-        return (num % 10) + somaDigitosRec(num / 10);
+    public static String inversorString(String str){
+        return inversorStringRec(str, str.length() - 1);
     }
 
     public static void main(String[] args){
@@ -33,10 +27,7 @@ public class somaDigitos{
         String linha = sc.nextLine();
         while(!(flegStop(linha))){
 
-            int num = stringParaInteiro(linha);
-            int soma = somaDigitosRec(num);
-
-            System.out.println(soma);
+            System.out.println(inversorString(linha));
 
             linha = sc.nextLine();
         }
